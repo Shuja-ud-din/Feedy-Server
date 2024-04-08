017a06f53716192177fe7fc2ecf30bd998b26ac7
Shuja ud Din <shuja1339@gmail.com>
2024-04-08 14:12:49 +0500

    get order by id

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -1,5 +1,8 @@
-import express from "express"
-import { adminAuthentication, authentication } from "../middlewares/authentication.js";
+import express from "express";
+import {
+  adminAuthentication,
+  authentication,
+} from "../middlewares/authentication.js";
 import Order from "../models/OrderModel.js";
 import getLastRecord from "../utils/getLastRecord.js";
 import User from "../models/UserModal.js";
@@ -8,173 +11,195 @@ import Product from "../models/ProductModel.js";
 const ordersRoutes = express.Router();
 
 ordersRoutes.get("/lastOrders", authentication, async (req, res) => {
-    try {
-        let orders = await Order.find({}).sort({ _id: -1 }).limit(5);
+  try {
+    let orders = await Order.find({}).sort({ _id: -1 }).limit(5);
 
-        if (!orders || orders.length === 0) {
-            throw new Error("No orders found");
-        }
-
-        orders = await Promise.all(orders.map(async (order) => {
-
-            const user = await User.findOne({ id: order.userId });
-
-            return {
-                id: order.id,
-                userId: order.userId,
-                userName: user.name,
-                status: order.status,
-                paymentMethod: order.paymentMethod,
-                createdAt: order.dateOfOrder,
-                totalPrice: order.totalPrice,
-            }
-        }));
-
-        res.json({
-            success: true,
-            message: "Newest 5 Orders",
-            data: orders.reverse(),
-        });
+    if (!orders || orders.length === 0) {
+      throw new Error("No orders found");
     }
-    catch (err) {
-        res.json({
-            success: false,
-            error: err.message
-        })
-    }
+
+    orders = await Promise.all(
+      orders.map(async (order) => {
+        const user = await User.findOne({ id: order.userId });
+
+        return {
+          id: order.id,
+          userId: order.userId,
+          userName: user.name,
+          status: order.status,
+          paymentMethod: order.paymentMethod,
+          createdAt: order.dateOfOrder,
+          totalPrice: order.totalPrice,
+        };
+      })
+    );
+
+    res.json({
+      success: true,
+      message: "Newest 5 Orders",
+      data: orders.reverse(),
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      error: err.message,
+    });
+  }
 });
 
 ordersRoutes.get("/allOrders", authentication, async (req, res) => {
-    try {
-        let orders = await Order.find({});
+  try {
+    let orders = await Order.find({});
 
-        if (!orders || orders.length === 0) {
-            res.json({
-                success: true,
-                message: "No orders found",
-                data: []
-            });
+    if (!orders || orders.length === 0) {
+      res.json({
+        success: true,
+        message: "No orders found",
+        data: [],
+      });
 
-            return;
-        }
-
-        orders = await Promise.all(orders.map(async (order) => {
-
-            const user = await User.findOne({ id: order.userId });
-
-            return {
-                id: order.id,
-                userId: order.userId,
-                userName: user.name,
-                phoneNumber: order.shippingDetails.phoneNumber,
-                adress: order.shippingDetails.address,
-                status: order.status,
-                dateOfDelivery: order.dateOfDelivery,
-                paymentMethod: order.paymentMethod,
-                createdAt: order.dateOfOrder,
-                totalPrice: order.totalPrice,
-            }
-        }))
-
-        res.json({
-            success: true,
-            message: "All orders",
-            data: orders
-        })
-
-    } catch {
-
+      return;
     }
-})
+
+    orders = await Promise.all(
+      orders.map(async (order) => {
+        const user = await User.findOne({ id: order.userId });
+
+        return {
+          id: order.id,
+          userId: order.userId,
+          userName: user.name,
+          phoneNumber: order.shippingDetails.phoneNumber,
+          adress: order.shippingDetails.address,
+          status: order.status,
+          dateOfDelivery: order.dateOfDelivery,
+          paymentMethod: order.paymentMethod,
+          createdAt: order.dateOfOrder,
+          totalPrice: order.totalPrice,
+        };
+      })
+    );
+
+    res.json({
+      success: true,
+      message: "All orders",
+      data: orders,
+    });
+  } catch {}
+});
 
 ordersRoutes.post("/createOrder", authentication, async (req, res) => {
-    const { userId, products, orderLocation, shippingDetails, paymentDetails = {
-        shippingPrice: 0,
-        taxPrice: 0
-    } } = req.body;
+  const {
+    userId,
+    products,
+    orderLocation,
+    shippingDetails,
+    paymentDetails = {
+      shippingPrice: 0,
+      taxPrice: 0,
+    },
+  } = req.body;
 
-    const user = await User.findOne({ id: userId });
+  const user = await User.findOne({ id: userId });
 
-    if (!user) {
-        res.json({
-            success: false,
-            error: "User not Found"
-        });
+  if (!user) {
+    res.json({
+      success: false,
+      error: "User not Found",
+    });
 
-        return;
+    return;
+  }
+
+  let lastOrder = await getLastRecord(Order);
+  let totalPrice = 0,
+    discount = 0;
+
+  let orderedProducts = await Product.find({
+    id: { $in: products.map((item) => item.productId) },
+  });
+
+  if (orderedProducts.length !== products.length) {
+    res.json({
+      success: false,
+      error: "Product not found",
+    });
+
+    return;
+  }
+
+  orderedProducts.forEach((product, index) => {
+    totalPrice = totalPrice + product.price * products[index].quantity;
+    discount = discount + product.discount * products[index].quantity;
+  });
+  orderedProducts = orderedProducts.map((product, index) => {
+    return {
+      productId: product.id,
+      quantity: products[index].quantity,
+      price: product.price,
+      discount: product.discount,
     };
+  });
 
-    let lastOrder = await getLastRecord(Order);
-    let totalPrice = 0, discount = 0;
-
-    let orderedProducts = await Product.find({ id: { $in: products.map(item => item.productId) } });
-
-    if (orderedProducts.length !== products.length) {
-        res.json({
-            success: false,
-            error: "Product not found"
-        })
-
-        return;
-    }
-
-    orderedProducts.forEach((product, index) => {
-        totalPrice = totalPrice + (product.price) * products[index].quantity;
-        discount = discount + product.discount * products[index].quantity;
-    });
-    orderedProducts = orderedProducts.map((product, index) => {
-        return {
-            productId: product.id,
-            quantity: products[index].quantity,
-            price: product.price,
-            discount: product.discount
-        }
+  try {
+    const orderDetails = await Order.create({
+      id: lastOrder ? lastOrder.id + 1 : 1,
+      userId,
+      userId,
+      orderLocation,
+      products: orderedProducts,
+      shippingDetails: {
+        location: shippingDetails.location,
+        email: shippingDetails.email,
+        phoneNumber: shippingDetails.phoneNumber,
+      },
+      paymentMethod: paymentDetails?.paymentMethod,
+      taxPrice: paymentDetails?.taxPrice,
+      shippingPrice: paymentDetails?.shippingPrice,
+      discount,
+      totalPrice:
+        totalPrice -
+        discount +
+        paymentDetails.taxPrice +
+        paymentDetails.shippingPrice,
     });
 
-    try {
-        const orderDetails = await Order.create({
-            id: lastOrder ? lastOrder.id + 1 : 1,
-            userId,
-            userId,
-            orderLocation,
-            products: orderedProducts,
-            shippingDetails: {
-                location: shippingDetails.location,
-                email: shippingDetails.email,
-                phoneNumber: shippingDetails.phoneNumber,
-            },
-            paymentMethod: paymentDetails?.paymentMethod,
-            taxPrice: paymentDetails?.taxPrice,
-            shippingPrice: paymentDetails?.shippingPrice,
-            discount,
-            totalPrice: totalPrice - discount + paymentDetails.taxPrice + paymentDetails.shippingPrice,
-        });
+    res.status(200).json({
+      success: true,
+      message: "Order was successfully placed",
+      data: {
+        orderId: orderDetails.id,
+        orderLocation: orderDetails.orderLocation,
+        shippingDetails: {
+          location: orderDetails.shippingDetails.location,
+          email: orderDetails.shippingDetails.email,
+          phoneNumber: orderDetails.shippingDetails.phoneNumber,
+        },
+        paymentMethod: orderDetails.paymentMethod,
+        discount,
+        taxPrice: orderDetails.taxPrice,
+        shippingPrice: orderDetails.shippingPrice,
+        totalPrice: orderDetails.totalPrice,
+      },
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
-        res.status(200).json({
-            success: true,
-            message: "Order was successfully placed",
-            data: {
-                orderId: orderDetails.id,
-                orderLocation: orderDetails.orderLocation,
-                shippingDetails: {
-                    location: orderDetails.shippingDetails.location,
-                    email: orderDetails.shippingDetails.email,
-                    phoneNumber: orderDetails.shippingDetails.phoneNumber,
-                },
-                paymentMethod: orderDetails.paymentMethod,
-                discount,
-                taxPrice: orderDetails.taxPrice,
-                shippingPrice: orderDetails.shippingPrice,
-                totalPrice: orderDetails.totalPrice,
-            }
-        })
-    }
-    catch (error) {
-        res.json({
-            success: false,
-            error: error.message,
-        })
-    }
-})
+ordersRoutes.get("/orders/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const order = await Order.findOne({ id: id });
+
+  res.json({
+    success: true,
+    message: "Order",
+    order,
+  });
+});
 
 export default ordersRoutes;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
-import express from "express"
-import nodemailer from "nodemailer"
+import express from "express";
+import nodemailer from "nodemailer";
 import User from "../models/UserModal.js";
 import jsonwebtoken from "jsonwebtoken";
 import generateToken from "../utils/generateToken.js";
@@ -10,193 +10,197 @@ import sendMail from "../utils/sendMail.js";
 const userRoutes = express.Router();
 
 userRoutes.post("/singup", async (req, res) => {
-    const { name, email, password, phoneNumber } = req.body;
+  const { name, email, password, phoneNumber } = req.body;
 
-    const emailExist = await User.findOne({ email });
-    const phNoExist = await User.findOne({ phoneNumber });
-    if (emailExist || phNoExist) {
-        res.status(201).json({
-            success: false,
-            message: `${emailExist ? "Email" : "Phone Number"} already Taken`
-        })
-        return
-    }
-
-    const lastUser = await getLastRecord(User);
-
-    const user = await User.create({
-        id: lastUser ? lastUser.id + 1 : 1,
-        name,
-        email,
-        password,
-        phoneNumber,
-        isVerifed: false,
-        role: "User",
-        otp: null
+  const emailExist = await User.findOne({ email });
+  const phNoExist = await User.findOne({ phoneNumber });
+  if (emailExist || phNoExist) {
+    res.status(201).json({
+      success: false,
+      message: `${emailExist ? "Email" : "Phone Number"} already Taken`,
     });
+    return;
+  }
 
-    const otp = Math.floor(1000 + Math.random() * 9000).toString();
+  const lastUser = await getLastRecord(User);
 
-    if (user) {
-        user.otp = otp;
-        await user.save();
-        sendMail(email, "OTP", otp, (error) => {
-            if (error) {
-                res.json({
-                    success: false,
-                    message: "Unable to send email.",
-                })
-                return;
-            }
+  const user = await User.create({
+    id: lastUser ? lastUser.id + 1 : 1,
+    name,
+    email,
+    password,
+    phoneNumber,
+    isVerifed: false,
+    role: "User",
+    otp: null,
+  });
+
+  const otp = Math.floor(1000 + Math.random() * 9000).toString();
+
+  if (user) {
+    user.otp = otp;
+    await user.save();
+    sendMail(email, "OTP", otp, (error) => {
+      if (error) {
+        res.json({
+          success: false,
+          message: "Email address not found.",
         });
-        res.status(200).json({
-            success: true,
-            message: "User added successfully",
-        })
-    }
-    else {
-        res.status(201).json({
-            success: false,
-            message: "User not added",
-        })
-    }
-})
+        return;
+      }
+    });
+    res.status(200).json({
+      success: true,
+      message: "User added successfully",
+    });
+  } else {
+    res.status(201).json({
+      success: false,
+      message: "User not added",
+    });
+  }
+});
 
 userRoutes.post("/signin", async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        res.status(202).json({
-            success: false,
-            message: "User not found"
-        })
-        return;
-    }
+  if (!user) {
+    res.status(202).json({
+      success: false,
+      message: "User not found",
+    });
+    return;
+  }
 
-    // if (!user.isVerifed) {
-    //     res.status(201).json({
-    //         message: "User not Verfied"
-    //     })
-    //     return;
-    // }
+  // if (!user.isVerifed) {
+  //     res.status(201).json({
+  //         message: "User not Verfied"
+  //     })
+  //     return;
+  // }
 
-    if (password === user.password) {
-        const token = jsonwebtoken.sign({
-            userId: user.id,
-            role: user.role,
-        }, process.env.JWT_SECRET);
-        res.status(200).json({
-            success: true,
-            user: {
-                name: user.name,
-                email: user.email,
-                role: user.role
-            },
-            token: token
-        })
-    }
-    else {
-        res.status(200).json({
-            success: false,
-            message: "Incorrect Password"
-        })
-    }
-
+  if (password === user.password) {
+    const token = jsonwebtoken.sign(
+      {
+        userId: user.id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET
+    );
+    res.status(200).json({
+      success: true,
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      token: token,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      message: "Incorrect Password",
+    });
+  }
 });
 
 userRoutes.post("/forgetPassword", async (req, res) => {
-    const { email } = req.body;
+  const { email } = req.body;
 
-    const user = await User.findOne({ email });
-    const tokenObj = await CPToken.findOne({ email });
+  const user = await User.findOne({ email });
+  const tokenObj = await CPToken.findOne({ email });
 
-    if (!user) {
-        res.status(200).json({
-            success: false,
-            message: "email not registered"
-        })
-        return;
-    }
-
-    const token = generateToken();
-
-    const changePasswordLink = `https://feedy-eta.vercel.app/SignIn/resetPassword/${user.id}/${token}`;
-
-    const mailOptions = {
-        from: process.env.MAIL_SENDER,
-        to: email,
-        subject: 'Change Password',
-        text: changePasswordLink
-    };
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.MAIL_SENDER,
-            pass: process.env.MAIL_PASS
-        }
+  if (!user) {
+    res.status(200).json({
+      success: false,
+      message: "email not registered",
     });
+    return;
+  }
 
-    const changeToken = async () => {
-        tokenObj.token = token;
-        tokenObj.expiration_time = Date.now() + 600000;
-        await tokenObj.save();
+  const token = generateToken();
+
+  const changePasswordLink = `https://feedy-eta.vercel.app/SignIn/resetPassword/${user.id}/${token}`;
+
+  const mailOptions = {
+    from: process.env.MAIL_SENDER,
+    to: email,
+    subject: "Change Password",
+    text: changePasswordLink,
+  };
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_SENDER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  const changeToken = async () => {
+    tokenObj.token = token;
+    tokenObj.expiration_time = Date.now() + 600000;
+    await tokenObj.save();
+  };
+
+  transporter.sendMail(mailOptions, async (error, info) => {
+    if (error) {
+      res.status(201).json({
+        success: false,
+        message: "Unable to send Mail",
+        error: error.message,
+      });
+    } else {
+      tokenObj
+        ? await changeToken()
+        : await CPToken.create({
+            userId: user.id,
+            token: token,
+            email,
+            expiration_time: Date.now() + 600000,
+          });
+      res.status(200).json({
+        success: true,
+        message: `Change Password Link sent to ${email}`,
+      });
     }
-
-    transporter.sendMail(mailOptions, async (error, info) => {
-        if (error) {
-            res.status(201).json({
-                success: false,
-                message: "Unable to send Mail",
-                error: error.message
-            })
-        } else {
-            tokenObj ?
-                await changeToken() : await CPToken.create({
-                    userId: user.id,
-                    token: token,
-                    email,
-                    expiration_time: Date.now() + 600000
-                })
-            res.status(200).json({
-                success: true,
-                message: `Change Password Link sent to ${email}`
-            });
-        }
-    });
-})
+  });
+});
 
 userRoutes.put("/resetPassword", async (req, res) => {
-    const { token, password, userId } = req.body;
+  const { token, password, userId } = req.body;
 
-    const tokenObj = await CPToken.findOne({ userId });
+  const tokenObj = await CPToken.findOne({ userId });
 
-    if (!tokenObj) {
-        res.status(201).json({
-            success: false,
-            message: "Invalid token"
-        })
-        return;
-    }
+  if (!tokenObj) {
+    res.status(201).json({
+      success: false,
+      message: "Invalid token",
+    });
+    return;
+  }
 
-    if (tokenObj.token === token && Date.now().toString() < tokenObj.expiration_time) {
-        const user = await User.findOne({ id: userId });
-        user.password = password;
-        await user.save();
-        await CPToken.deleteOne({ userId });
-        res.status(200).json({
-            success: true,
-            message: "Password Changed Successfully"
-        })
-    } else {
-        await CPToken.deleteOne({ userId });
-        res.status(201).json({
-            success: false,
-            message: "Token Expired"
-        })
-    }
-})
+  if (
+    tokenObj.token === token &&
+    Date.now().toString() < tokenObj.expiration_time
+  ) {
+    const user = await User.findOne({ id: userId });
+    user.password = password;
+    await user.save();
+    await CPToken.deleteOne({ userId });
+    res.status(200).json({
+      success: true,
+      message: "Password Changed Successfully",
+    });
+  } else {
+    await CPToken.deleteOne({ userId });
+    res.status(201).json({
+      success: false,
+      message: "Token Expired",
+    });
+  }
+});
 
 export default userRoutes;
